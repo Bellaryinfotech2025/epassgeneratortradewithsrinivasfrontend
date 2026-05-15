7da6570562f25ab71674843be98e16783f594fa6
Bellaryinfotech <bellaryinfotechsolutions@gmail.com>
2026-05-16 03:06:47 +0530

    commit on the epas generator

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-     port: 5173,    
-     
+    port: 5173,
+    allowedHosts: [
+      'tradewithsrinivas.bellaryinfotech.com',
+      'tradewithsrinivas.bellaryinfotech.com',
+    ],
+    proxy: {
+      '/api': {
+        target: 'https://tradeepass.bellaryinfotech.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
   }
 })
